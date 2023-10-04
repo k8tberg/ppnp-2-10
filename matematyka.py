9358d7fc696854ba02a5e118f491cf617595fea3
k8tberg <k8tberg@gmail.com>
2023-10-04 09:51:17 +0200

    day 2 petle 2

---
+++ matematyka.py
@@ -0,0 +1,13 @@
+import math
+
+pi = math.pi
+print(pi)  # 3.141592653589793
+
+print(math.sqrt(625))  # 25.0 - float
+print(math.cos(50))  # 0.9649660284921133
+print(math.pow(1.7 * 10 ** 308, 1))  # 1.7e+308 max liczba
+# print(math.pow(1.8 * 10 ** 308, 1)) # przekroczony limit mozliwosci obliczania  --> inf
+
+print(math.pow(2, 10))  # 1024.0 - pow - potęgowanie
+
+
