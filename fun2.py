fd67a8368fb65fc6593275cca06a3965056678da
k8tberg <k8tberg@gmail.com>
2023-10-04 12:45:30 +0200

    day 3 fun2:

--- fun2.py
+++ fun2.py
@@ -3,7 +3,22 @@
 def dodaj(a, b):
     return a + b
 
+
+def oblicz_vat(cena, vat=23):
+    return cena * (100 + vat) / 100
+
+
 print(dodaj(5, 7))
 wyn = dodaj(5, 6)
 print(wyn)
-print(dodaj(7, 8) + dodaj(8, 88)) #111
+print(dodaj(7, 8) + dodaj(8, 88))  # 111
+
+print(oblicz_vat(1000))
+print(oblicz_vat(1000, 7))
+print(oblicz_vat(vat=15, cena=5000))
+
+zm = oblicz_vat(1000)  # 1230.0
+if zm == 1230:
+    print("Działa")  # działa
+
+list.
