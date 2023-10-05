4a234feeba4bbab4d41ab08bce9cc084d689fd04
k8tberg <k8tberg@gmail.com>
2023-10-05 14:16:26 +0200

    d 4: json_zad1 update

--- json_zad1.py
+++ json_zad1.py
@@ -13,8 +13,19 @@ print(type(dict_json))
 with open('nasze_dane.json', 'w') as f:
     json.dump(person_dict, f, indent=4, sort_keys=True) #zapisanie danych
 
+#odczytanie jsona z pliku do słownika
 with open('nasze_dane.json', 'r') as f:
     data = json.load(f)
 
 print(data)
 print(type(data))
+# wypisać itemy, klucze, wartości
+
+print(data.keys())
+print(data.values())
+print(data.items())
+# dict_keys(['age', 'czy_pali', 'name'])
+# dict_values([40, None, 'Radek'])
+# dict_items([('age', 40), ('czy_pali', None), ('name', 'Radek')])
+print(data['name']) #Radek
+
