6f00ae8fea2f1147c338b11e4c4e72b699b5b4ef
k8tberg <k8tberg@gmail.com>
2023-10-06 13:41:28 +0200

    d5 : bazy3 update

--- baza3.py
+++ baza3.py
@@ -6,10 +6,26 @@ import sqlite3
 try:
     sql_connection = sqlite3.connect('sqlite_python.db')
     insert_sql = "INSERT INTO software (id, name, price) VALUES (1,'Python','1000000);"
+    select = '''
+    SELECT * FROM software;
+    '''
+    update = '''
+    UPDATE software SET price = 2000 
+    WHERE id=1;
+    '''
+
+    delete = '''
+    DELETE FROM software WHERE id=1;
+    '''
     cursor = sql_connection.cursor()
     print("Baz danych została podłączona")
-    cursor.execute(insert_sql)
+    # cursor.execute(insert_sql)
+    # sql_connection.commit()
+    # cursor.execute(update)
+    cursor.execute(delete)
     sql_connection.commit()
+    for row in cursor.execute(select):
+        print(row) #select nie wymaga commita
 
 except sqlite3.Error as e:
     print("Błąd podczas podłączanie bazy danych")
